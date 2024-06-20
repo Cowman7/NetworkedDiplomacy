1480b5a942181998f0f44e86f9a1e22bde62a190
Nelson Cowan <cowannb17@uww.edu>
2024-06-20 00:49:48 -0500

    Added function to create window but not show it

--- Window/MainWindow.java
+++ Window/MainWindow.java
@@ -29,5 +29,17 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
+    public void create() {
+        setTitle(title);
+
+        setSize(600, 500);
+        setMinimumSize(new Dimension(400, 300));
+
+        
+        setIconImage(new ImageIcon(getClass().getResource("favicon.png")).getImage());
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 }
 
